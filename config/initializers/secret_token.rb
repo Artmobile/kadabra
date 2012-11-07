cca3850d7dd58f6c19afd33d387505eb2a7575e6
Artmobile <artmobile@gmail.com>
2012-11-07 13:08:54 +0200

    Initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kadabra::Application.config.secret_token = 'bb6df62e0ed63e40f13aa60b37766b1d89c76fe0c3f53e78574e557f1dab0d322d1b867ba18e94d2761c79dab2bd18450cb6e216ba0775c620002b02111e6019'
